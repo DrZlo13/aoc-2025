7af64875672ea3290474d4ec0d37328129ce8166
SG <who.just.the.doctor@gmail.com>
2025-12-02 11:58:00 +0000

    day 2, part 1

---
+++ aoc02-1/src/main.rs
@@ -0,0 +1,56 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+const INPUT_FILE: &str = "input.txt";
+
+fn process_pair(start: i64, end: i64) -> i64 {
+    let mut sum = 0;
+
+    for num in start..=end {
+        let num_str = num.to_string();
+        if num_str.len() % 2 != 0 {
+            continue;
+        }
+
+        let mid = num_str.len() / 2;
+        let first_half = &num_str[..mid];
+        let second_half = &num_str[mid..];
+        if first_half == second_half {
+            sum += num;
+        }
+    }
+
+    sum
+}
+
+fn process_line(line: &str) -> i64 {
+    let mut sum = 0;
+
+    let parts = line.split(",");
+    for part in parts {
+        let pair = part.trim();
+        let nums: Vec<&str> = pair.split("-").collect();
+        let start = nums[0].parse::<i64>().unwrap();
+        let end = nums[1].parse::<i64>().unwrap();
+
+        sum += process_pair(start, end);
+    }
+
+    sum
+}
+
+fn main() -> std::io::Result<()> {
+    let file_path = Path::new(INPUT_FILE);
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    let mut sum = 0;
+    for line in reader.lines().flatten() {
+        sum += process_line(&line);
+    }
+
+    println!("Total sum: {}", sum);
+
+    Ok(())
+}
